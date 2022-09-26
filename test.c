a697198144bef32206270543b1c8958960d646f1
Matias Colil <matias.colil@usach.cl>
2022-09-26 10:26:32 -0300

    test

---
+++ test.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include <string.h>
+int main(int argc, char const *argv[])
+{
+    char *palabra = "5000";
+    char string[100];
+    FILE *archivo = fopen("datos_juegos.csv", "r");
+    int pos;
+    int temp;
+    int anteriorSalto = 0;
+    long t = 261;
+    while (fgets(string, 100, archivo) != NULL)
+    {
+        printf("%s \n", string);
+        if (strstr(string, palabra) != 0)
+        {
+            printf("Encontro el año\n");
+            temp = ftell(archivo);
+            pos = anteriorSalto;
+            printf("%d\n", temp);
+            // printf("%s \n",string);
+        }
+        anteriorSalto = ftell(archivo);
+    }
+
+    printf("%d\n", pos);
+    fseek(archivo, pos, SEEK_SET);
+    fgets(string, 100, archivo);
+    printf("STRING SETEADO: %s\n", string);
+
+    return 0;
+}
